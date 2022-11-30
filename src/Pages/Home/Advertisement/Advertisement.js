87b983cad1b3490bb6a3cd37f1f9771ccafac0e5
wpmdalamin <mdalamin7g@gmail.com>
2022-11-30 20:37:21 +0600

    Advertisement

--- src/Pages/Home/Advertisement/Advertisement.js
+++ src/Pages/Home/Advertisement/Advertisement.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ProductCard from '../AllProducts/ProductCard/ProductCard';
 
 const Advertisement = () => {
+    const advertisement = 'true';
+    const [advertisementData, setAdvertisementData] = useState([])
+    useEffect(() => {
+        fetch(`http://localhost:5000/ads-product?advertisement=${advertisement}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log("advertisement products ", data)
+                setAdvertisementData(data);
 
-    // useEffect(() => {
-    //     fetch()
-    //     .then(res => res.json())
-    //     .then(data => console.log(data))
-    // },[])
+            })
+    }, [])
 
     return (
         <div className='my-5'>
-            <h3 className='text-3xl text-center'>Advertisement</h3>
+            {
+                advertisementData.length > 0 &&
+                <>
+                    <h3 className='text-3xl text-center'>Advertisement</h3>
+                    <div className='grid grid-cols-3 gap-8 my-5'>
+                        {
+                            advertisementData.map(product => <ProductCard
+                                key={product._id}
+                                product={product}
+                            ></ProductCard>)
+                        }
+                    </div>
+
+                </>
+            }
+
         </div>
     );
 };
